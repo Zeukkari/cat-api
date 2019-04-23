644a33cb0beb6dbdab648f860765417675045b14
Timo Aho <zeukkari@gmail.com>
2019-04-24 02:32:30 +0300

    refactor

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use(express.static('static'))
+app.use(express.static('client/build'))
 
 app.use(
   '/graphql',
