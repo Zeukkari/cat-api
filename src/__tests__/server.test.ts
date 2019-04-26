7fae431d4482095904ca242376265790964baf1e
Timo Aho <zeukkari@gmail.com>
2019-04-26 06:09:32 +0300

    fix test

--- src/__tests__/server.test.ts
+++ src/__tests__/server.test.ts
@@ -28,7 +28,7 @@ describe('Test the root path', () => {
   })
 
   test('It should response the GET method', async () => {
-    const response = await request(app).get('/');
+    const response = await request(app).get('/api');
     expect(response.statusCode).toBe(200);
   });
 
