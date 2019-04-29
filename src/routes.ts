e0ca7d1a2b8ff1b843cf9884e49e1f6360d41b0a
Timo Aho <zeukkari@gmail.com>
2019-04-29 23:17:56 +0300

    test utils for vscode + more tests

--- src/routes.ts
+++ src/routes.ts
@@ -1,70 +1,72 @@
-
 import { Sequelize } from 'sequelize-typescript';
 import { Cats } from './models/Cat';
+
 const Op = Sequelize.Op;
 
 export async function list(req, res, next) {
-  try {
-    const cats = await Cats.findAll()
-    res.status(200).json(cats);
-  } catch (e) {
-    res.status(400).send(e)
-  }
+  const cats = await Cats.findAll()
+  res.status(200).json(cats);
 }
 
 export async function search(req, res, next) {
   try {
-    const cats = await Cats.findAll({
-      limit: 10,
-      where: {
-        name: {
-          [Op.like]: `%${req.query.search}%`,
-        }
-      }
-    })
-    res.status(200).json(cats);
-  }
-  catch (e) {
-    res.status(400).send(e)
+    if (!req.query.search) {
+      throw {code: 400, msg: 'Search query missing'}
+    } else {
+        const cats = await Cats.findAll({
+          limit: 10,
+          where: {
+            name: {
+              [Op.like]: `%${req.query.search}%`,
+            }
+          }
+        })
+        res.status(200).json(cats);
+    }
+  } catch(error) {
+    res.status(400).json({error})
   }
 }
 
-export async function get(req, res, next) {
+export async function get(req, res) {
   try {
     const cat = await Cats.findByPk(req.params.catId)
-    res.status(200).json(cat);
+    res.status(200).json(cat)
   }
   catch (e) {
-    res.status(400).send(e)
+    res.status(400).json({error: 'get failed'})
   }
 }
 
-export async function create(req, res, next) {
+export async function create(req, res) {
   try {
-    const cats = await Cats.create({
-      description: req.body.description,
-      name: req.body.name,
-      origin: req.body.origin,
-      temperament: req.body.temperament,
-    })
-    res.status(201).json(cats);
-  }
-  catch (e) {
-    next(e)
+    if (!req.body.name) {
+      throw {code: 400, msg: 'Property `name` is missing'}
+    } else {
+      const cats = await Cats.create({
+        description: req.body.description,
+        name: req.body.name,
+        origin: req.body.origin,
+        temperament: req.body.temperament,
+      })
+      res.status(201).json(cats);
+    }
+  } catch(e) {
+    res.status(400).json({error: 'create failed'})
   }
 }
 
-export async function remove(req, res, next) {
+export async function remove(req, res) {
   try {
-    const catId = req.params.catId
     await Cats.destroy({
       where: {
-        id: catId
+        id: req.params.catId
       }
     })
-    res.status(201).json({deleted: catId});
+    res.status(201).json({deleted: req.params.catId});
+
   }
   catch (e) {
-    next(e)
+    res.status(400).json({error: 'delete failed'})
   }
 }
